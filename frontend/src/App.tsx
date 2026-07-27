3c5c1f343fba41039abecd1dc46d20421bf3d219
xiev <nikitakoltamenkov@proton.me>
2026-07-28 02:54:35 +0300

    баловство

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
-import './App.css'
+import "@/App.css"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <header>
+          <div>Aniki</div>
+
+          <Input placeholder="Найти..." />
+
+          <Button>Profile</Button>
+      </header>
+
+        <aside>
+
+        </aside>
+
+        <main>
+            <div className="bg-primary text-primary-foreground">Hello</div>
+        </main>
+
+        <footer>
+
+        </footer>
     </>
   )
 }
